6a0cab4b0539d03a924de28c6db82eec85881274
johannakaipainen <72135377+johannakaipainen@users.noreply.github.com>
2022-02-01 15:43:29 +0200

    finished ui

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Announcement from '../components/Announcement';
+import Categories from '../components/Categories';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import NewsLetter from '../components/NewsLetter';
+import Products from '../components/Products';
+import Slider from '../components/Slider';
+
+export default function Home() {
+  return <div>
+    <Announcement />
+    <Navbar />
+    <Slider/>
+    <Categories />
+    <Products />
+    <NewsLetter />
+    <Footer/>
+  </div>;
+}
